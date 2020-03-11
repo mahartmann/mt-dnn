607048512f7cc9bff8ef69b8305024318422242a
Mareike <mrkhartmann4@gmail.com>
2020-03-11 13:26:42 +0100

    added pcs metric for negscope detection

--- data_utils/metrics.py
+++ data_utils/metrics.py
@@ -49,6 +49,37 @@ def compute_seqacc(predicts, labels, label_mapper):
     report = classification_report(y_true, y_pred,digits=4)
     return report
 
+def compute_pcs(predicts, labels, label_mapper):
+    """
+    compute correctly predicted full spans
+    :param predicts:
+    :param labels:
+    :return:
+    """
+    y_true, y_pred = [], []
+    def trim(predict, label):
+        temp_1 = []
+        temp_2 = []
+        for j, m in enumerate(predict):
+            if j == 0:
+                continue
+            if label_mapper[label[j]] != 'X' and label_mapper[label[j]] != 'CLS' and label_mapper[label[j]] != 'SEP':
+                temp_1.append(label_mapper[label[j]])
+                temp_2.append(label_mapper[m])
+        temp_1.pop()
+        temp_2.pop()
+        y_true.append(temp_1)
+        y_pred.append(temp_2)
+    tp = 0.
+    for predict, label in zip(predicts, labels):
+        trim(predict, label)
+        if predict == label:
+            tp += 1
+    return tp/len(predicts)
+
+
+
+
 def compute_emf1(predicts, labels):
     return evaluate_func(labels, predicts)
 
@@ -62,6 +93,7 @@ class Metric(Enum):
     AUC = 5
     SeqEval = 7
     EmF1 = 8
+    PCS = 9
 
 
 
@@ -73,7 +105,8 @@ METRIC_FUNC = {
  Metric.Spearman: compute_spearman,
  Metric.AUC: compute_auc,
  Metric.SeqEval: compute_seqacc,
- Metric.EmF1: compute_emf1
+ Metric.EmF1: compute_emf1,
+ Metric.PCS: compute_pcs
 }
 
 
@@ -88,6 +121,8 @@ def calc_metrics(metric_meta, golds, predictions, scores, label_mapper=None):
         if mm in (Metric.ACC, Metric.F1, Metric.MCC):
             metric = metric_func(predictions, golds)
         elif mm == Metric.SeqEval:
+            metric = metric_func(predictions, golds, label_mapper)
+        elif mm == Metric.PCS:
             metric = metric_func(predictions, golds, label_mapper)
         elif mm == Metric.EmF1:
             metric = metric_func(predictions, golds)
